57cc364e8753073b788895e341a3fe489198acef
hikikomorime <michal@skweres.net>
2012-07-08 00:31:27 +0200

    uper

--- project.js
+++ project.js
@@ -2,6 +2,7 @@ angular.module('project', ['mongolab']).
   config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ListCtrl, templateUrl:'list.html'}).
+      when('/list', {controller:ListCtrl, templateUrl:'list.html'}).
       when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
       when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
       otherwise({redirectTo:'/'});
